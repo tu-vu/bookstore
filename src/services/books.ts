187f8099e24d0f404baa0cbfa44b07afd726f861
Tu Vu <tuvu852@gmail.com>
2022-01-08 18:06:54 -0600

    Update base url

--- src/services/books.ts
+++ src/services/books.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:3000/api/books";
+const baseUrl = "http://localhost:3001/books";
 
 const getAll = async () => {
   const response = await axios.get(baseUrl);
